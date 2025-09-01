6697915c43c19ca4a87a66e80c5e35fc7d220ae6
Sudharsan <sudharsan123@gmail.com>
2025-09-02 01:52:22 +0530

    d

--- server/models/User.js
+++ server/models/User.js
@@ -1,16 +1,12 @@
 import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schema(
-  {
-    _id: { type: String, required: true },
-    name: { type: String, required: true },
-    email: { type: String, required: true, unique: true },
-    image: { type: String, default: "" },
-  },
-  { timestamps: true }
-);
+const userSchema =new mongoose.Schema({
+    _id:{type:String, required:true},
+    name:{type:String, required:true},
+    email:{type:String, required:true},
+    image:{type:String, required:true},
+})
 
-// Prevent model overwrite in dev environments
-const User = mongoose.models.User || mongoose.model("User", userSchema);
+const User =mongoose.model('User',userSchema)
 
 export default User;
