0d010a3d2f4706cb18ba0bba7def94eca8f0cda7
Sudharsan <sudharsan123@gmail.com>
2025-09-02 15:55:31 +0530

    d

--- server/models/User.js
+++ server/models/User.js
@@ -1,12 +1,16 @@
 import mongoose from "mongoose";
 
-const userSchema =new mongoose.Schema({
-    _id:{type:String, required:true},
-    name:{type:String, required:true},
-    email:{type:String, required:true},
-    image:{type:String, required:true},
-})
+const userSchema = new mongoose.Schema(
+  {
+    _id: { type: String, required: true }, // Clerk ID
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    image: { type: String, required: true },
+  },
+  { timestamps: true } 
+);
 
-const User =mongoose.model('User',userSchema)
+// Avoid OverwriteModelError on hot-reload
+const User = mongoose.models.User || mongoose.model("User", userSchema);
 
 export default User;
